c0ebc1afa6fcff93ea06cf77ab1032111a112984
Erwin <erwin18@mhs.akba.ac.id>
2024-06-27 01:42:28 +0800

    lagi ngawur

--- application/models/M_jadwal.php
+++ application/models/M_jadwal.php
@@ -1,5 +1,21 @@
 <?php
 
+
+// Turn off error reporting
+error_reporting(0);
+
+// Report runtime errors
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+// Report all errors
+error_reporting(E_ALL);
+
+// Same as error_reporting(E_ALL);
+ini_set("error_reporting", E_ALL);
+
+// Report all errors except E_NOTICE
+error_reporting(E_ALL & ~E_NOTICE);
+
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class M_jadwal extends CI_Model
