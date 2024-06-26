1ffbe6241d542857ae865f8159108b39f2a80330
Erwin <erwin18@mhs.akba.ac.id>
2024-06-27 00:33:22 +0800

    lagi ngawur

--- application/config/database.php
+++ application/config/database.php
@@ -73,12 +73,34 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => '127.0.0.1:3307',
+// 	'username' => 'root',
+// 	'password' => '',
+// 	'database' => 'e-schedule',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '127.0.0.1:3307',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'e-schedule',
+	'hostname' => 'localhost',
+	'username' => 'hvsteofj_root',
+	'password' => '@Erwin2024#',
+	'database' => 'hvsteofj_jadwalku',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
@@ -90,7 +112,7 @@ $db['default'] = array(
 	'swap_pre' => '',
 	'encrypt' => FALSE,
 	'compress' => FALSE,
-	'stricton' => FALSE,
+	'stricton' => TRUE,
 	'failover' => array(),
 	'save_queries' => TRUE
 );
