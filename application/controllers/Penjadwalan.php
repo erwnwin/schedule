1a47f6a49f7b4ecf25dfe457a74e417405bbb3d4
erwnwin <erwin18@mhs.akba.ac.id>
2024-08-26 03:07:58 +0800

    Cobaaaaa

--- application/controllers/Penjadwalan.php
+++ application/controllers/Penjadwalan.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);  // Report all types of errors and warnings
 defined('BASEPATH') or exit('No direct script access allowed');
 
